0a1a97bae5f85450fbed2530cbf7132c44d61c39
swann14444 <SwannL>
2020-11-11 11:19:19 +0100

    fix conflict

--- analyse.py
+++ analyse.py
@@ -5,7 +5,6 @@ file = open("dataCovid.csv", "r")
 valuesHospi = OrderedDict()
 valuesRea = OrderedDict()
 valuesDc = OrderedDict()
-<<<<<<< HEAD
 valuesHospi_lorraine = OrderedDict()
 valuesRea_lorraine = OrderedDict()
 valuesDc_lorraine = OrderedDict()
@@ -23,7 +22,6 @@ for line in file:
       valuesRea[parsedLine[1]] = int(parsedLine[3])
       valuesDc[parsedLine[1]] = int(parsedLine[4])
     if parsedLine[0] in lorraine:
-        print("ping")
         try:
             valuesHospi_lorraine[parsedLine[1]] += int(parsedLine[2])
             valuesRea_lorraine[parsedLine[1]] += int(parsedLine[3])
@@ -35,23 +33,6 @@ for line in file:
 
 def plot_week_mean_hospi():
     figure("Nouvelles admissions journalieres moyenne à l'hopital sur une semaine")
-=======
-file.readline()
-
-for line in file:
-  parsedLine = line.split(";")
-  try:
-    valuesHospi[parsedLine[1]] += int(parsedLine[2])
-    valuesRea[parsedLine[1]] += int(parsedLine[3])
-    valuesDc[parsedLine[1]] += int(parsedLine[4])
-  except:
-    valuesHospi[parsedLine[1]] = int(parsedLine[2])
-    valuesRea[parsedLine[1]] = int(parsedLine[3])
-    valuesDc[parsedLine[1]] = int(parsedLine[4])
-
-def plotHospi():
-    figure("Nouvelles admissions journalieres a l'hopital")
->>>>>>> 2ec85ec28e5604286ea45a262da1e4c64c6b2c25
     x, y = zip(*valuesHospi.items())
     new_x = x[::7]
     new_y = [0] * len(new_x)
@@ -165,7 +146,6 @@ def plot_week_mean_rea_lorraine():
         label.set_visible(True)
 
 
-<<<<<<< HEAD
 def plot_week_mean_dc_lorraine():
     figure("Nouveaux déces moyen sur une semaine lorraine")
     x, y = zip(*valuesDc_lorraine.items())
@@ -181,34 +161,13 @@ def plot_week_mean_dc_lorraine():
     new_y[-1] = new_y[-1] * last__week_fix
 
     _ = matplotlib.pyplot.plot(new_x, new_y)
-=======
 
-def plot_week_mean_dc():
-    figure("Nouveau déces sur une semaine")
-    x, y = zip(*valuesDc.items())
-    new_x = x[::7]
-    new_y = [0]*len(new_x)
-    last_week_fix = 0
-    for i in range(len(y)):
-        # une semaine va de 0 à 6 modulo 7 (semaine de 7 à 13, 14 à 20 ...)
-        #last_week_fix vaut dont 7/nombre de jour ecoulé sur la semaine en cours. Cela permet d'extrapolé les rea/hospi de la semaine en cours
-        jours_ecoulé = 7*(i/7 - i//7)+1
-        last__week_fix = 7/jours_ecoulé if jours_ecoulé!=0 else 0        
-
-        new_y[int(i/7)] += y[i]
-    new_y[-1] = new_y[-1] * last__week_fix
-        
-        
-    _ = matplotlib.pyplot.plot(new_x,new_y)
->>>>>>> 2ec85ec28e5604286ea45a262da1e4c64c6b2c25
     ax = matplotlib.pyplot.gca()
     matplotlib.pyplot.xticks(rotation=90)
     for label in ax.get_xaxis().get_ticklabels()[::]:
         label.set_visible(False)
     for label in ax.get_xaxis().get_ticklabels()[::2]:
         label.set_visible(True)
-
-<<<<<<< HEAD
 
 
 plot_week_mean_hospi()
@@ -217,12 +176,5 @@ plot_week_mean_dc()
 plot_week_mean_hospi_lorraine()
 plot_week_mean_rea_lorraine()
 plot_week_mean_dc_lorraine()
-=======
-plotHospi()
-plotRea()
-plot_week_mean_hospi()
-plot_week_mean_rea()
-plot_week_mean_dc()
->>>>>>> 2ec85ec28e5604286ea45a262da1e4c64c6b2c25
 
 show()
